41d686d26660ce6c55177af97f9b2b3352a24cf0
jianghuafei <jianghuafei12@163.com>
2020-11-19 17:17:50 +0800

    [修改]demo逻辑优化

--- flance-gateway-demo/src/main/java/com/flance/gateway/client/GatewayAuthClient.java
+++ flance-gateway-demo/src/main/java/com/flance/gateway/client/GatewayAuthClient.java
@@ -4,7 +4,7 @@ import com.flance.gateway.client.fallback.GatewayAuthClientFallBack;
 import com.flance.web.gateway.client.AuthClient;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "flance-auth", path = "/permission", fallbackFactory = GatewayAuthClientFallBack.class)
+@FeignClient(name = "flance-auth", contextId = "permission", path = "/api/permission", fallbackFactory = GatewayAuthClientFallBack.class)
 public interface GatewayAuthClient extends AuthClient {
 
 }
