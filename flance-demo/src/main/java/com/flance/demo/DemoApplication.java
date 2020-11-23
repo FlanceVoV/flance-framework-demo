e2676004a4624e17e325fc7bdb989ab3c0b5688c
jianghuafei <jianghuafei12@163.com>
2020-11-23 15:36:47 +0800

    [新增]增加题库模块引用

--- flance-demo/src/main/java/com/flance/demo/DemoApplication.java
+++ flance-demo/src/main/java/com/flance/demo/DemoApplication.java
@@ -9,9 +9,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.flance"})
-@EnableJpaRepositories(basePackages = {"com.flance.demo.domain","com.flance.components.fastdfs.domain","com.flance.components.form.domain"})
-@EntityScan(basePackages = {"com.flance.demo.domain","com.flance.components.fastdfs.domain","com.flance.components.form.domain"})
+@ComponentScan(basePackages = {"com.flance.*"})
+@EnableJpaRepositories(basePackages = {"com.flance.*.domain","com.flance.components.*.domain"})
+@EntityScan(basePackages = {"com.flance.*.domain","com.flance.components.*.domain"})
 public class DemoApplication {
 
     public static void main(String[] args) {
